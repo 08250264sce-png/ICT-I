529fc196b52798b99838b6397349fc2667e3a59d
08250264sce-png <08250264.sce@rub.edu.bt>
2026-03-20 15:55:04 +0530

    logical operator

--- LogicalOpe.py
+++ LogicalOpe.py
@@ -1,10 +1,10 @@
 x = True
 y = False
 result_and = x and y
-print("Logical AND:",result_and)
+print("Logical AND:", result_and)
 
 result_or = x and y
-print("Logical OR:",result_or)
+print("Logical OR:", result_or)
 
 result_not = x and y
-print("Logical NOT:",result_not)
+print("Logical NOT:", result_not)
