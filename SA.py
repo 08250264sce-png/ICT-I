88047a567adb9f327654933f044e95974ab2883f
08250264sce-png <08250264.sce@rub.edu.bt>
2026-03-20 16:20:33 +0530

    Avtivity

---
+++ SA.py
@@ -0,0 +1,17 @@
+name = input ("Enter your name:")
+num1 = float(input("Enter the num1:"))
+num2 = float(input("Enter the num2"))
+addition = num1 + num2
+print("addition;",addition)
+subtraction = num2 + num2
+print("subtraction:",subtraction)
+multiplication = num1 * num2
+print("multiplication:",multiplication)
+division = num1 / num2
+print("division:",division)
+print("Hello",name)
+print("Thank you")
+
+
+
+
